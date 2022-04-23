f85694853cbcf80456300b34d3953ccfea27dabd
divya1677 <94949861+divya1677@users.noreply.github.com>
2022-04-23 16:29:03 +0530

    Delete if loop.c

--- 4_TestCases/if loop.c
+++
@@ -1,8 +0,0 @@
-if(pwd[0]=='4'&&pwd[1]=='5'&&pwd[2]=='6'&&pwd[3]=='7')
-	{
-			
-			lcdcmd(0x01);
-			lcddis("  ACCESS GRANTED   ", 19);
-			lcdcmd(0xc0);  //To pass to next line
-			lcddis("   WELCOME HOME!   ", 20);
-			i = 50;
